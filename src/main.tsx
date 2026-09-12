732d15841d24307a4c66aba7c05aa7ac86a2e14f
oguzhanozeren92-debug <oguzhanozeren92@gmail.com>
2026-09-12 15:05:43 +0300

    Unify mobile screens with clean white app theme

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import './styles/TarlaPusulaTheme.css';
 import './styles/MobileAppShell.css';
+import './styles/WhiteAppTheme.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
